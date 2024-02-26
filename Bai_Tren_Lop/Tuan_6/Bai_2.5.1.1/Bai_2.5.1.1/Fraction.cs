3a5709d8e94d943eb7ec5e5aad7995744db01f0a
Quy Dai <stu715105034@hnue.edu.vn>
2024-02-26 21:50:04 +0700

    Bo sung them

---
+++ Bai_Tren_Lop/Tuan_6/Bai_2.5.1.1/Bai_2.5.1.1/Fraction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2._5._1._1
+{
+    internal class Fraction
+    {
+        public int TuSo { get; set; }
+        public int MauSo { get; set; }
+        public Fraction() { }
+        public Fraction(int tuSo, int mauSo)
+        {
+            TuSo = tuSo;
+            MauSo = mauSo;
+        }
+        public void nhapDL()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập vào tử số:");
+            TuSo = int.Parse(Console.ReadLine());
+            Console.Write("Nhập vào mẫu số:");
+            MauSo = int.Parse(Console.ReadLine());
+        }
+        public int UCLN(int a , int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        public void rutGon()
+        {
+            int ucln = UCLN(TuSo, MauSo);
+            TuSo /= ucln;
+            MauSo /= ucln;
+        }
+        public void xuatDL()
+        {
+            rutGon();
+            Console.WriteLine($"Phân số:{TuSo}/{MauSo}");
+        }
+        public static Fraction operator+(Fraction a,Fraction b)
+        {
+            int tuSo = a.TuSo * b.MauSo + b.TuSo * a.MauSo;
+            int mauSo = a.MauSo * b.MauSo;
+            return new Fraction(tuSo, mauSo);
+        }
+        public static bool operator ==(Fraction a , Fraction b)
+        {
+            return a.TuSo * b.MauSo == b.TuSo * a.MauSo;
+        }
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return a.TuSo * b.MauSo == b.TuSo * a.MauSo;
+        }
+    }
+}
---
+++ Bai_Tren_Lop/Tuan_6/Bai_2.5.1.1/Bai_2.5.1.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2._5._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction ps1 = new Fraction();
+            ps1.nhapDL();
+            Fraction ps2 = new Fraction();
+            ps2.nhapDL();
+            Fraction ps3 = ps1 + ps2;
+            ps3.xuatDL();
+            if(ps1 == ps2)
+            {
+                Console.WriteLine("2 Phân số bằng nhau");
+            }
+            else
+            {
+                Console.WriteLine("2 phân số không bằng nhau");
+            }
+        }
+    }
+}
